e0806211957fe301804236a1bbdb879bd1b59304
mahadihasan2 <mahadihasan125643@gmail.com>
2022-04-01 15:06:08 +0600

    Components-work-done

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import CustomLink from '../CustomLink/CustomLink';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <h1>T-shirt Maniya</h1>
+            <nav>
+               <CustomLink to="/">Home</CustomLink>
+               <CustomLink to="/orderreview">Order Review</CustomLink>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
